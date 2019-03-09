ad7bbc35d1dd9caa6e2e8be0b0f26b5fd3cccfce
Filip Havrlent <filip@havrlent.com>
2019-03-09 15:11:08 +0100

    Backup script

--- backup.sh
+++ backup.sh
@@ -2,19 +2,21 @@
 
 REPOSITORY=~/Backup
 
-borg create -v --stats                          \
-    $REPOSITORY::'{hostname}-{now:%Y-%m-%d-%HH-%MM}'    \
+/usr/local/bin/borg create -v --stats                          \
+    $REPOSITORY::'{hostname}-{now:%Y-%m-%d-%HH-%MM-%SS}'    \
     ~/.bash_profile \
     ~/.bashrc \
     ~/.ssh \
     ~/.vscode \
     ~/.zshrc \
     ~/Documents \
-    ~/Movies \
     ~/Pictures \
+    ~/Movies \
+    ~/Music \
     ~/repos \
-    ~/Disk\ Google \
     --exclude */node_modules
     
-borg prune -v --list $REPOSITORY --prefix '{hostname}-' \
+/usr/local/bin/borg prune -v --list $REPOSITORY --prefix '{hostname}-' \
     --keep-hourly=24 --keep-daily=7 --keep-weekly=4 --keep-monthly=6
+
+rclone copy $REPOSITORY gbackup:
